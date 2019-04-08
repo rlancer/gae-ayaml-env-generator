83e094aa702805672eafc9b58310f549d073cf62
robert <rob@collaborizm.com>
2019-04-08 16:37:30 -0400

    fixed resolve issules

--- src/controller.ts
+++ src/controller.ts
@@ -1,6 +1,6 @@
 import YAML from 'yaml'
 import fs from 'fs'
-import { } from '@oclif/command';
+import path from 'path'
 
 type TParams = {
     appYamlTemplatePath: string
@@ -10,11 +10,21 @@ type TParams = {
 export function generate({ appYamlTemplatePath, envPrefix }: TParams): string {
     const KEY_PREFIX = 'APP_'
 
-    const parsed = YAML.parse(fs.readFileSync('app.template.yaml', 'utf8'))
+    console.log(__dirname)
+    console.log(path.resolve(appYamlTemplatePath))
+
+    const parsed = YAML.parse(fs.readFileSync(path.resolve(appYamlTemplatePath), 'utf8')) || {}
+
+
 
     const newEnv: Record<string, string> = {}
 
     Object.keys(process.env).filter(key => key.startsWith(KEY_PREFIX)).map(key => newEnv[key.replace(KEY_PREFIX, '')] = process.env[key] as string)
+
+    if (!parsed['env_variables']) {
+        parsed['env_variables'] = {}
+    }
+
     Object.assign(parsed['env_variables'], newEnv)
 
     const file = YAML.stringify(parsed)
